be95c68b3ad02ebed2618fb74da6ee95b6af9b90
Abhishek Pandit <abhishekpandit986@gmail.com>
2024-09-11 05:29:20 +0000

    added

---
+++ Opp javascript/Claculator.js
@@ -0,0 +1,30 @@
+class Calculator{
+    a;
+    b;
+    constructor(a,b){
+        this.a = a;
+        this.b = b;
+    }
+    add(num1,num2){
+        this.a = num1
+        this.b = num2
+        return this.a + this.b
+    }
+    subStrcat(){
+        return this.a - this.b
+    }
+    divide(){
+        return this.a - this.b
+    }
+    multiply(){
+
+    }
+}
+
+let addNumber = new Calculator()
+// console.log(addNumber)
+console.log(addNumber.add(10,25))
+console.log(addNumber)
+// console.log(addNumber.subStrcat())
+// console.log(addNumber.divide())
+
